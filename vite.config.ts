5a9e0e2497853ef92373ebf89645824e8edd7279
aine-sax <yukitobaotian@gmail.com>
2025-05-30 01:21:49 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/precision-metronome/',  // ← リポジトリ名をここに！
+  base: '/precision-metronome/',
   plugins: [react()],
 })
+
 
